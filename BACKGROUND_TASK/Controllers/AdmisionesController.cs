7972b073cd2907233c6d60d0476cf7f62b0f0bf0
Delfry Paulino Ortega <delfry_paulino@ucne.edu.do>
2024-07-12 01:59:36 -0400

    Controllers

---
+++ BACKGROUND_TASK/Controllers/AdmisionesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
+using Shared.Repositories;
+
+namespace BACKGROUND_TASK.Controllers;
+
+[Route("api/admisiones")]
+[ApiController]
+public class AdmisionesController(ICache<Admisiones> _admisionesCache) : ControllerBase
+{
+    [HttpGet]
+    public IEnumerable<Admisiones> Get()
+    {
+        return _admisionesCache.ListoOfData!;
+    }
+}
---
+++ BACKGROUND_TASK/Controllers/NoBackController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
+using Shared.Repositories;
+
+namespace BACKGROUND_TASK.Controllers;
+
+[Route("api/back")]
+[ApiController]
+public class NoBackController(IApiService<Admisiones> service) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IEnumerable<Admisiones>> Get()
+    {
+        return await service.GetAll();
+    }
+}
